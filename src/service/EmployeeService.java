7959341ae1ea0547352a9869d2d03e917bb3e022
surajDambhare <surajdambhare28@gmail.com>
2026-06-03 19:14:39 +0530

    Added validation

--- src/service/EmployeeService.java
+++ src/service/EmployeeService.java
@@ -2,6 +2,9 @@ package service;
 
 public class EmployeeService {
     public void getEmployees(){
+        if(true) {
+            System.out.println("Validation success");
+        }
         System.out.println("Fetching employee!");
     }
 }
