3d3412f734c3e96f37b8fe59d43951bdebe8c8f0
surajDambhare <surajdambhare28@gmail.com>
2026-06-03 19:12:39 +0530

    Added logging in employee service

--- src/service/EmployeeService.java
+++ src/service/EmployeeService.java
@@ -2,6 +2,7 @@ package service;
 
 public class EmployeeService {
     public void getEmployees(){
+        System.out.println("Logging employee request");
         System.out.println("Fetching employee!");
     }
 }
